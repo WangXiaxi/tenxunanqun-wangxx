6ed4aff36e88008694adcc5e0591229ea9f31d06
王小星 <995107408@qq.com>
2016-08-18 20:17:32 +0800

    cre

---
+++ dev/js/study.js
@@ -0,0 +1,183 @@
+var sub1 = {"code":0,"data":[{"description":"守卫QQ 神器来袭\n\t\t\t",
+			"title":"",
+			"date":"2014-09-23"},{"description":"QQ设备锁，保障你的帐号安全\n\t\t\t","title":"","date":"2014-08-11"},{"description":"QQ设备锁 别让心爱的人因你落泪\n\t\t\t","title":"","date":"2014-05-28"},{"description":"新骗局：警惕补手机卡盗QQ！\n\t\t\t","title":"","date":"2013-07-16"},{"description":"游戏锁：零负担、威力强\n\t\t\t","title":"","date":"2013-05-13"},{"description":"游戏锁怎么用？\n\t\t\t","title":"","date":"2013-05-13"},{"description":"QQ安全防护模块 贴身保护帐号安全\n\t\t\t","title":"","date":"2013-04-16"},{"description":"绑密保，领豪华礼包\n\t\t\t","title":"","date":"2012-11-14"},{"description":"安全达人等级体系 一站式帮助\n\t\t\t","title":"","date":"2012-05-24"},{"description":"QQ被盗，怎么办\n\t\t\t","title":"","date":"2012-03-01"},{"description":"密保忘了、丢了怎么办？\n\t\t\t","title":"","date":"2011-11-03"},{"description":"如何解锁和锁定QQ帐号","title":"","date":"2011-10-26"},{"description":"QQ令牌使用教程\n\t\t\t","title":"","date":"2011-09-15"},{"description":"QQ令牌，安全随身带\n\t\t\t","title":"","date":"2011-09-08"},{"description":"“举报功能”指引\n\t\t\t","title":"","date":"2011-04-27"},{"description":"QQ安全中心向用户拜年\n\t\t\t","title":"","date":"2011-01-27"},{"description":"Q币Q点保护设置技巧\n\t\t\t","title":"","date":"2010-09-08"},{"description":"[视频]密保手机教程\n\t\t\t","title":"","date":"2010-09-01"},{"description":"设置强密码提高帐号安全\n\t\t\t","title":"","date":"2010-08-26"},{"description":"手机令牌博客征文活动\n\t\t\t","title":"","date":"2010-08-19"},{"description":"帐号申诉技巧\n\t\t\t","title":"","date":"2010-08-04"},{"description":"如何避免帐号发生异常\n\t\t\t","title":"","date":"2010-07-28"},{"description":"QQ安全中心手机版使用教程\n\t\t\t","title":"","date":"2010-03-29"}],"message":"success"};
+var sub2 = {"code":0,"data":[{"description":"QQ新增“安全登录”模式  加固帐号安全\n\t\t\t",
+			"date":"2014-04-15"},{"description":"财务人员不能不防的“画皮”诈骗\n\t\t\t","date":"2014-04-14"},{"description":"看看身边的勇士都在哪？\n\t\t\t","date":"2014-04-10"},{"description":"手机丢了，绑定的QQ还安全吗？\n\t\t\t","date":"2014-03-06"},{"description":"电脑管家推出“帐号宝”，向钓鱼盗号利益集团宣战\n\t\t\t","date":"2014-01-22"},{"description":"诈骗案例早知道，安安全全过新年\n\t\t\t","date":"2014-01-20"},{"description":"低价Q币、土豪金，你上当了吗？\n\t\t\t","date":"2013-11-28"},{"description":"电话来了，兜里的钱跑了？\n\t\t\t","date":"2013-11-28"},{"description":"扫个二维码，银行卡里的钱不翼而飞\n\t\t\t","date":"2013-11-21"},{"description":"公司财务QQ成盗号者新宠\n\t\t\t","date":"2013-11-14"},{"description":"丢失的Q群和好友，可以恢复啦\n\t\t\t","date":"2013-11-14"},{"description":"扫个二维码，被骗三千元？\n\t\t\t","date":"2013-11-06"},{"description":"假冒客服混水摸鱼？！\n\t\t\t","date":"2013-10-18"},{"description":"你被钓鱼了吗？\n\t\t\t","date":"2013-09-25"},{"description":"警惕'网吧大盗'，专盗网吧QQ\n\t\t\t","date":"2013-09-25"},{"description":"店铺首页惊现“被冻结”？！\n\t\t\t","date":"2013-09-12"},{"description":"卖家不得不防的“钓鱼陷阱”\n\t\t\t","date":"2013-07-12"},{"description":"警惕要给你“打钱”的兄弟\n\t\t\t","date":"2013-04-18"},{"description":"“加群网赚、日赚万元”是陷阱！\n\t\t\t","date":"2013-01-10"},{"description":"使用官方版本，杜绝QQ外挂\n\t\t\t","date":"2012-10-31"},{"description":"开学季！谨防校园诈骗！\n\t\t\t","date":"2012-09-19"},{"description":"防骗小助手--QQ被盗篇\n\t\t\t","date":"2012-07-25"},{"description":"亿万用户携手反诈骗\n\t\t\t","date":"2012-07-17"},{"description":"网络购票陷阱多 节日出行需警惕\n\t\t\t","date":"2012-01-10"},{"description":"我们一起走过的日子\n\t\t\t","date":"2012-01-09"},{"description":"恶意网址举报系统，举报恶意网址\n\t\t\t","date":"2012-01-05"},{"description":"网络诈骗大盘点\n\t\t\t","date":"2011-12-29"},{"description":"“吸费”诈骗来袭，hold住！\n\t\t\t","date":"2011-12-13"},{"description":"官方短信查询系统，谨防短信诈骗\n\t\t\t","date":"2011-11-30"},{"description":"都是“跳转”惹的祸\n\t\t\t","date":"2011-11-11"},{"description":"留学生视频诈骗专题\n\t\t\t","date":"2011-10-20"},{"description":"中奖诈骗识别方法\n\t\t\t","date":"2011-09-21"},{"description":"系列专题一：视频诈骗\n\t\t\t","date":"2011-09-07"},{"description":"访问“危险网站”需谨慎\n\t\t\t","date":"2011-07-20"},{"description":"[测试]您是否容易上当？\n\t\t\t","date":"2011-06-23"},{"description":"切勿在“非腾讯官方网站”输入QQ帐号及密码\n\t\t\t","date":"2011-01-27"},{"description":"谨防“假机票网站”\n\t\t\t","date":"2011-01-07"},{"description":"系列专题四：钓鱼网址\n\t\t\t","date":"2010-09-08"},{"description":"系列专题三：中奖欺诈\n\t\t\t","date":"2010-09-08"},{"description":"系列专题二：冒充好友骗钱\n\t\t\t","date":"2010-09-08"},{"description":"漫画：免费的午餐\n\t\t\t","date":"2010-08-18"},{"description":"一招制敌，识破诈骗邮件\n\t\t\t","date":"2010-07-07"},{"description":"如何识别假冒客服电话\n\t\t\t","date":"2010-03-29"},{"description":"如何识别诈骗邮件\n\t\t\t","date":"2010-03-29"}],"message":"success"};	
+var sub3 = {"code":0,"data":[{"description":"养成安全的上网习惯\n\t\t\t","date":"2011-06-23"},{"description":"网吧安全使用QQ的建议\n\t\t\t","date":"2011-06-23"},{"description":"四个步骤保护电脑安全\n\t\t\t","date":"2011-06-23"},{"description":"修复QQ软件漏洞和操作系统漏洞\n\t\t\t","date":"2011-06-23"},{"description":"聊天消息中的网址安全吗？\n\t\t\t","date":"2011-12-05"},{"description":"QQ安全组件 保护帐号安全\n\t\t\t","date":"2011-12-05"},{"description":"什么是“不在常用地区登陆”？\n\t\t\t","date":"2011-12-05"},{"description":"当QQ提示发现盗号木马后，应该如何处理？\n\t\t\t","date":"2011-12-05"},{"description":"成功接收文件后，如何处理“.重命名”文件？\n\t\t\t","date":"2011-12-05"},{"description":"为什么在我聊QQ时，系统消息提示“被迫下线”？\n\t\t\t","date":"2011-12-05"}],"message":"success"};
+var notice = {"code":0,"data":[{"description":"部分用户登录PC QQ提示安全扫一扫和一键验证未绑定的解决方案  (20160229)"},{"description":"关于QQ登录保护升级为设备锁以及部分功能下线的通知  (20150825)"},{"description":"部分用户无法成功登录QQ的通知 (20150806)"},{"description":"部分用户无法成功更换密保手机的通知 (20150526)"},{"description":"紧急通知：少量用户使用密保手机可能无法收到验证码短信，重试可成功 (20150313)"},{"description":"重要通知：QQ改密系统升级，暂时无法改密，预计8月6日上午8点恢复 (20140805)"},{"description":"手机令牌KJava和Symbian版本停止绑定 (20140730)"},{"description":"紧急通知：电信手机用户于2014年4月10日凌晨无法使用 (20140409)"},{"description":"密保卡停止服务通知：请升级至新方案 (20140122)"},{"description":"密保手机短信号1066170099将暂停接收短信上行(20131030)"},{"description":"密保手机支持港、澳、台以及美国地区号码，开放免费下发短信渠道 (20131030)"},{"description":"公告：密保卡产品停止绑定通知 (20130617)"},{"description":"紧急通知：部分联通用户无法正常发送短信到QQ安全中心 (20130617)"},{"description":"解除安全模式新体验：让您和“收不到验证码”的痛苦说再见(20130529)"},{"description":"紧急通知：联通用户无法正常收到短信 (20130409)"},{"description":"紧急通知：部分电信用户无法正常收到短信 (20130121)"},{"description":"紧急通知：联通用户无法正常发送短信到QQ安全中心 (20130116)"},{"description":"密保大作战之年末之战活动公告 (201201226)"},{"description":"Q币Q点保护新特性：安全扫一扫，识别钓鱼更安全 (201201106)"},{"description":"公告：手机令牌Kjava2.0版停止下载 (201201024)"},{"description":"公告：手机密保卡停止下发 (20120705)"},{"description":"密保手机新体验：让您和“收不到验证码”的痛苦说再见(20120613)"},{"description":"QQ安全中心三周年活动公告(20120518)"},{"description":"DNF的勇士们：解除安全模式推出GW短信指令啦(20120509)"}],"message":"success"};
+var startText = (function($){
+	$.fn.extend({
+		createDOM : function(data,sel,page){
+			var len = data.length;
+			var allPage = Math.ceil(len / page);
+			$(this).find(".pub"+sel).append(
+				"<p class=\"cla\">专题</p>"+
+				"<div class=\"page\"></div>"+
+				"<div class=\"MainInText\"><ul></ul></div>"
+			);
+			for(var j = 0;j < allPage;j++){
+				$(this).find(".pub"+sel+" .page").append(
+					"<em>"+ (j + 1) +"</em>"
+				)
+			}
+			for(var i = 0;i < len;i++){
+				$(this).find(".pub"+sel+" .MainInText ul").append(
+					"<li>"+
+						"<a class=\"description\">"+ data[i].description +"</a>"+
+						"<span class=\"date\">"+ data[i].date +"</span>"+
+					"</li>"
+				);
+			}
+		},
+	 	noticeAddInner : function(){
+			var data = notice.data.slice(0,16);
+			for(var i = 0;i < data.length;i++){
+
+				$(this).append("<li>"+
+								"<a>"+data[i].description+"</a>"+
+								"</li>");
+			}
+		},
+		mouseAd : function(page,select){
+			var playLi = function(page,sel,fisrtNum){
+					sel.css("display","none");
+					for(var i = 0; i < page;i++){
+						sel.eq(i+fisrtNum).css("display","block");
+					}
+				}
+			var $prev = $(this).find(".pub" + select + " .page em");
+			$prev.mouseenter(function(){
+				var fisrtNum = $(this).index()*page;
+				var sel = $(this).parent().siblings(".MainInText").find("li");
+				playLi(page,sel,fisrtNum);
+				$(this).addClass("cur").siblings().removeClass("cur");
+			});
+			$prev.eq(0).mouseenter();
+		}
+	});
+	$(".notice").noticeAddInner();
+	var start = function(data1,data2,data3,index1,index2,index3,page1,page2,page3){
+		$(".textIn-right").createDOM(data1,index1,page1);
+		$(".textIn-right").createDOM(data2,index2,page2);
+		$(".textIn-right").createDOM(data3,index3,page3);
+		$(".textIn-right").mouseAd(page1,index1);
+		$(".textIn-right").mouseAd(page2,index2);
+		$(".textIn-right").mouseAd(page3,index3);
+	}
+	return start;
+})(jQuery);
+
+		var imgs = [{imgSrc:"https://aq.qq.com/v2/images/topics/my_prot_bns_promotion.jpg"},
+					{imgSrc:"https://aq.qq.com/v2/images/topics/my_prot_2d_code_promotion.jpg"},
+					{imgSrc:"http://img3.imgtn.bdimg.com/it/u=1169388607,391079864&fm=206&gp=0.jpg"},
+					{imgSrc:"http://img0.imgtn.bdimg.com/it/u=415825692,13297482&fm=206&gp=0.jpg"}];
+		(function($,imgs){
+			var 
+				$width = 620,
+				len = imgs.length,
+				animated = 0,
+				stop;
+			function createDOM(){
+				
+				$(".bannerGai").append("<div class=\"container\"></div>"+
+										"<div class=\"buttons\">"+
+											"<img src=\"img/num2.png\" class = \"imgBg\"/>"+
+										"</div>"
+						);
+				$(".container").append("<a><img src=\"" +imgs[len-1].imgSrc + "\" /></a>");
+				for(var i = 0;i < len;i++){
+					$(".container").append("<a><img src=\"" +imgs[i].imgSrc + "\" /></a>");
+					$(".buttons").append("<em></em>");
+				}
+				$(".container").append("<a><img src=\"" +imgs[0].imgSrc + "\" /></a>");
+				$(".buttons em").eq(0).addClass("cur");
+				$("img.imgBg").width(30 + len*16 + "px");
+				var left1 = ($width / 2) - ($("img.imgBg").width() / 2) + "px";
+				var conWidth = $width*(len+2) + "px";
+				$("img.imgBg").css("left",left1);
+				$(".container").css("width",conWidth);
+			}
+			function banner(offset){
+				if (animated) {
+					return;
+				}
+				animated = 1;
+
+				$(".container").animate({
+					left : parseInt($(".container").css("left")) + offset
+				},400,function(){
+						animated = 0;
+						var wid = parseInt($(".container").css("left"));
+						if (wid < -len*$width) {
+						$(".container").css("left","-" + $width + "px");
+					}else if (wid > -1*$width) {
+						$(".container").css("left","-" + len*$width + "px");
+					}
+					
+				});
+				
+			}
+			function play(){
+				stop = setInterval(function(){
+				banner(-$width);
+				setIndex(1);
+			},5000);
+			}
+			function setIndex(jue){
+				var index = Math.abs(parseInt($(".container").css("left")) / $width) - 1 + jue;
+					console.log(index);
+					if (index == len) {
+						index = 0;
+					}else if (index == -1) {
+						index = len-1;
+					}
+					console.log(len);
+					console.log(index);
+				$(".buttons em").eq(index).addClass("cur").siblings().removeClass("cur");
+			}
+
+			function clickSpan(){
+				
+				$(".bannerGai span.perv").click(function(){
+					clearInterval(stop);
+					if (animated) {
+					return;
+					}
+					banner($width);
+					setIndex(-1);
+					play();
+				});
+				$(".bannerGai span.next").click(function(){
+					clearInterval(stop);
+					if (animated) {
+					return;
+					}
+					banner(-$width);
+					setIndex(1);
+					play();
+				});
+			}
+
+			function clickEm(){	
+				$(".buttons em").click(function(){
+					if (animated) {
+					return;
+					}
+					animated = 1;
+					clearInterval(stop);
+					$(this).addClass("cur").siblings().removeClass("cur");
+					var leftWidth = "-" + ($(this).index())*$width;
+					$(".container").animate({
+						left : leftWidth
+					},400,function(){
+						animated = 0;
+					});
+					play();
+				});
+			}
+			
+			createDOM();
+			clickEm();
+			play();
+			clickSpan();
+		})(jQuery,imgs);
